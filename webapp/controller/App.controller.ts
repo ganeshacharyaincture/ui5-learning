7f9a615e7528ea8bbec9cd7e4145e0b593cc612b
Ganesh Acharya <ganesh.acharya@incture.com>
2026-02-20 11:21:28 +0530

    day-4

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,6 +1,17 @@
 import Controller from 'sap/ui/core/mvc/Controller';
+import JSONModel from 'sap/ui/model/json/JSONModel';
 
 export default class AppController extends Controller {
+  onInit(): void | undefined {
+    const oData = {
+      recipient: {
+        name: "Ganesh Acharya"
+      }
+    };
+    const oModel = new JSONModel(oData);
+    this.getView()?.setModel(oModel);
+
+  }
   onShowHello(): void {
     alert("Hello");
   }
